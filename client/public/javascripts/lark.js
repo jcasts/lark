4172e70e5bfc593992ff3df304afeef488114af7
jcastagna <jeremie.castagna@gmail.com>
2012-02-28 18:28:01 -0800

    fillout

--- client/public/javascripts/lark.js
+++ client/public/javascripts/lark.js
@@ -2,7 +2,7 @@ Lark = {
   init: function(){
     var loaded = false;
 
-    Lark.GUI.preload(function(name, finished){
+    Lark.View.preload(function(name, finished){
       loaded = finished;
     });
 
@@ -13,6 +13,7 @@ Lark = {
 
 
   error: function(message){
+    logger.error(message);
   },
 
 
@@ -26,7 +27,10 @@ Lark = {
       data: data,
 
       statusCode: {
-        200: callback;
+        200: function(data){
+          eval("var data = " + data);
+          callback(data);
+        }
 
         400: function(){
           Lark.error("Invalid client request");
@@ -55,7 +59,7 @@ Lark = {
 
     return Lark.request("post", "/session", data, function(data){
       // Set cookie
-      callback();
+      callback(data);
     })
     .error(function(){
       Lark.error("Invalid login");
@@ -69,7 +73,7 @@ Lark = {
     return Lark.request("delete", "/session/"+Lark.session, {}, function(data){
       // Erase cookie
       // Reload html page
-      callback();
+      callback(data);
     })
     .error(function(){
       Lark.error("Could not logout");
@@ -81,7 +85,7 @@ Lark = {
 }
 
 
-Lark.GUI = {
+Lark.View = {
 
   cache: {
     pages: {},
@@ -97,17 +101,17 @@ Lark.GUI = {
 
 
   display_page: function(page){
-    Lark.GUI.elmt.content.html(Lark.GUI.cache.pages[page]);
+    Lark.View.elmt.content.html(Lark.View.cache.pages[page]);
   },
 
 
   preload_pages: function(callback){
     var loaded = 0;
-    var pages  = Lark.GUI.page_names;
+    var pages  = Lark.View.page_names;
 
     for(var i=0; i < pages.lenth; i++){
       $.get('pages/' + pages[i] + '.html', function(data){
-        Lark.GUI.cache.pages[pages[i]] = data;
+        Lark.View.cache.pages[pages[i]] = data;
         loaded++;
         callback(pages[i], loaded === pages.length);
       })
@@ -123,28 +127,132 @@ Lark.GUI = {
   },
 
 
-  // Saves GUI state to local storage
+  // Saves View state to local storage
   save_state: function(){
   }
 }
 
 
+Lark.Contact = {
+
+  // Create a new contact. Supports attributes:
+  //  name:   contact name
+  //  emails: list of emails for that person
+  //  tags:   tag that person!
+  create: function(attribs, callback){
+    return Lark.request("post", "/contact", attribs, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to create contact");
+    });
+  },
+
+
+  // Delete a contact. Requires a contact id.
+  delete: function(id, callback){
+    return Lark.request("delete", "/contact/"+id, {}, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to delete contact");
+    });
+  },
+
+
+  // Update a contact. Supports create attributes and requires a contact id.
+  update: function(id, attribs, callback){
+    return Lark.request("put", "/contact/"+id, attribs, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to update contact");
+    });
+  },
+
+
+  // Read and return the contact by its id.
+  get: function(id, callback){
+    return Lark.request("get", "/contact/"+id, {}, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to retrieve contact");
+    });
+  }
+
+
+  // Get a list of contacts. Supports query params:
+  //  count:    number to fetch
+  //  start:    start index
+  //  filters:  filter list to apply during retrieval
+  //    terms:    search terms
+  //    fields:   fields to search: name, emails, tags
+  list: function(query, callback){
+    return Lark.request("get", "/contact", query, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to get contacts");
+    });
+  },
+}
+
+
 Lark.Mail = {
 
-  cache: {
-    emails: {}
-  },
-
-
-  compose: function(){
-  },
-
-
   receive: function(callback){
+    return Lark.request("get", "/popmail", {}, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to receive POP email");
+    });
   },
 
 
   send: function(email, callback){
+    return Lark.request("post", "/email", email, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to send email");
+    });
+  },
+
+
+  // Read and return the email by its id.
+  get: function(id, callback){
+    return Lark.request("get", "/email/"+id, {}, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to retrieve email");
+    });
+  },
+
+
+  // Delete an email. Requires an email id.
+  delete: function(id, callback){
+    return Lark.request("delete", "/email/"+id, {}, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to delete contact");
+    });
+  },
+
+
+  // Update an email thread. Requires an email id and supports these attributes:
+  //  tags:   a list of tags for the email
+  //  folder: the folder this email should live in (inbox, archive, spam, trash)
+  update: function(id, attribs, callback){
+    return Lark.request("put", "/contact/"+id, attribs, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to update email");
+    });
   },
 
 
@@ -153,8 +261,15 @@ Lark.Mail = {
   //  start:    start index
   //  filters:  filter list to apply during retrieval
   //    terms:    search terms
-  //    fields:   fields to search: body, subject, sender
+  //    fields:   fields to search: body, subject, sender, tags, date
+  //  folders:  name of the folder to search (inbox, archive, spam, trash, all)
   list: function(query, callback){
+    return Lark.request("get", "/email", query, function(data){
+      callback(data);
+    })
+    .error(function(){
+      Lark.error("Failed to read email");
+    });
   },
 
 
